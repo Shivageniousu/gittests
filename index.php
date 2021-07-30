c84eb193c8cae9468e3c824f61039d3a6a3048ea
Shivageniousu <87021319+Shivageniousu@users.noreply.github.com>
2021-07-30 18:50:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (isset($_REQUEST['utm_medium'])) {
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Scale Up Entrepreneurs Masterclass</title>
+  <title>Scale Up feature</title>
   <link rel="stylesheet" href="css/foundation.css">
   <link rel="stylesheet" href="css/app.css">
   <link rel="stylesheet" href="css/iceberg.css">
